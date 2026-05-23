71152b1329a2c90dd02dd3d139b432cbea8c1c23
aungphonep502-byte <aungphonep502@gmail.com>
2026-05-23 16:27:25 +0630

    python-learning-day-1

---
+++ exercise.py
@@ -0,0 +1,12 @@
+a = input("your name")
+b = int(input("your age"))
+c = input("your current city")
+
+print (f"Hello,{a},your name is {a},city is {c} your age is {c}")
+
+your_age = int(input("your age!"))
+will_be = 10 + your_age
+print("In next 10 years, u will be", will_be)
+
+fav_sub = input("Tell me your fav sub!")
+print("your fav sub is", fav_sub)
---
+++ new.py
@@ -0,0 +1,440 @@
+# name = input("what's your name?")
+# print("hello,", name)
+
+# name = input("what's your name?")
+# print("your name is", name,".")
+
+# name = input ("what's your name?")
+# print ("your name is", name)
+
+# name = input("what's your name?")
+# print("your name is")
+# print(name)
+
+
+# name = input("what's your name?")
+# print("Hello,", name) 
+
+# name = input("what's your name?")
+# print("Is your name is", name, "?") 
+
+# name = input("what's your name?")
+# print ("hello,", end ="")
+# print(name)
+
+# name = input("what's your name?")
+# print("your name is", end="\n")
+# print(name)
+
+# name = input("what's your name?")
+# print ("your name is", end = "")
+# print (name) 
+
+# name = input("what's your name?")
+# print("So, your name's", end="\n")
+# print(name)
+
+# print ("Hello, \"friends\"")
+
+# print ('Hello, "friends"')
+
+# print ('hello, "friends"')
+# print ("hello, \"friends\"")
+
+# name = input("aungaung")
+# print ("hello", name)
+
+# name = input ("what is your name?")
+# print (f"hello, {name}")
+
+# name = input ("what is your name?")
+# age = 25 
+# print (f"{name} is {age} years old")
+
+# print ("hello, \"friends\"")
+
+# print ('hello, "friends"')
+
+# name = "aung"
+# print("hello,", name)
+
+# name ="aung"
+# print(f"hello,{name}")
+
+# age = 23 
+# print(f"my age is {age}")
+
+# phone_device = "iPhone"
+# print(f"my phone is {phone_device}")
+
+# name = input("what phone du using currently?")
+# print(f"currently, i am using {name}")
+
+
+# age = input("how old are u?")
+# age = age.strip()
+# print(f"Your age is {age}")
+
+# name = input("What's your name?")
+# name = name.title().strip()
+# print(f"Your name is {name}.")
+
+# hobby = input("What is your hobby?").strip().title()
+# print(f"Your hobby is {hobby}?")
+
+# a = int(input("enter a number"))
+# b = int(input("enter a number"))
+# c=a+b
+# print(c)
+
+# a = input("type a numeber")
+# b = input ("type a number")
+# c= int(a) + int(b)
+# print (c)
+
+# a = float (input("type a number!"))
+# b = float (input("type a numebr"))
+# c= a + b 
+# print(c)
+
+# a = float(input("type one number!"))
+# b = float(input("type a number"))
+# c = round(a*b)
+# print(c)
+
+# a=float(input("type a numebr!"))
+# b=float(input("type a number"))
+# c=round(a*b,3)
+# print(c)
+
+# a = float(input("type a number"))
+# b = float(input("type a number"))
+# c=round(a/b,2)
+# print(c)
+
+# a = float(input("type a number!"))
+# b = float(input("type a number!"))
+# c = round(a * b) 
+# print (c)
+
+# a = float(input("type a number!"))
+# b = float(input("type a number!"))
+# # c = round(a*b,2)
+# print(f"{c:.2f}") 
+
+# def hello():
+#     print("Hello")
+
+# hello()
+
+# def hello(to="world"):
+#     print("hello,", to)
+
+# hello("david")
+# hello()
+
+# def hello(to="world"):
+#     print("hello", to)
+
+#     def main():
+#         name=input("what is your name?")
+#         main(name)
+
+# def hello(to="world"):
+#         print("hello,", to)
+# def main():
+#     name = input("what is your name?")
+#     hello(name)
+#     hello()
+# main()
+
+
+# def main():
+
+#     name = input("What's your name? ")
+#     hello(name)
+
+#     hello()
+
+
+# def hello(to="world"):
+#     print("hello,", to)
+
+
+# main()
+
+# def main():
+#     name = input("Type your name?")
+#     hello(name)
+#     hello()
+
+# def hello(to="world"):
+#     print("Hello,", to)
+    
+# main()
+
+# def fat(calorie):
+#     print("You burned", calorie,"calories")
+# a= input("calorie")
+# fat(a)
+
+# def username(to="aung phone paing"):
+#     print("Your name is",to,"!")
+
+# name1 = input("Type your name!")
+# username(name1)
+
+# def hello(to="world"):
+#     print("hello,",to) 
+
+# name=input("what is your name?")
+# hello(name)
+# hello()
+
+# def hello():
+#     print("hello")
+# hello()
+
+# def hello():
+#     print("hello!")
+# hello()
+
+# def hello(to):
+#     print("hello",to)
+# hello("app")
+
+# def hello(to):
+#     print(to)
+# hello("andrew")
+
+# def hello(to="world"):
+#     print(to)
+# hello()
+# hello("app")
+
+# def main():
+#     a = input("type your name!")
+#     hello(a)
+#     hello()
+
+# def hello(to="app"):
+#     print("hello",to)
+    
+# main()
+
+# def main():
+#     a = input("WHAT IS your name?")
+#     hello(a)
+#     hello()
+
+# def hello(to="yati"):
+#   print("hello,",to)
+
+# main ()
+
+# def main():
+#     x =int(input("type a number!"))
+#     print("a square is", square(x))
+
+# def square (n):
+#     return n * n
+# main()
+
+# def main():
+#     a = int(input("type a number!"))
+#     print("square of this number is", square(a))
+
+# def square(v):
+#     return v * v
+
+# main ()
+
+# your_name = input("Type your name!")
+# your_age  = input("Type your age!")
+# city      = input("Type your city")
+# print("your name is",your_name)
+# print("your age is", your_age)
+# print("your city is", city)
+
+# your_name =input("Type your name!")
+# your_age = input("Type your age!")
+
+# print(f"Hello, {your_name} your age is {your_age}!")
+
+# x = "aung phone paing"
+# y = 13
+# z = 1.56
+
+# print(x,y,z)
+
+# x = str(15)
+# y = int (15)
+# z = float(15)
+# print (x,y,z)
+
+# x = 5 
+# y = "aung aung"
+# print = x
+
+# a = 4 
+# a= "app" 
+# print (a) 
+
+# a = str(10)
+# b = int(20)
+# c = float(30)
+# print (a, b, c)
+
+# a = 5
+# b = "John"
+
+# print (type(a))
+# print (type(b))
+
+# a = "app"
+# print (a)
+
+# a = 'bae'
+# print (a)
+
+# a = "app"
+# A = 'ytst'
+# print (a,A)
+
+
+# myVariableName = "app"
+# MyVariableName = "app"
+# my_variable_name = 'app'
+
+# a,b,c = 1,2,3
+# print (a)
+# print (b)
+# print (c)
+
+# a,b,c = "aung", "phone", "paing"
+# print (a,b,c) 
+
+# a=b=c ='Andrew'
+# print (a)
+# print (b)
+# print (c)
+
+# a =['app','bae','lu','azay']
+# x,y,z,b =a
+# # print (b)
+
+# print (x,y,b)
+
+# a = "you are"
+# b = "so"
+# c = 'strong'
+# # print(a + b + c)
+# print (a,b,c)
+
+
+# a = 123
+# b = 'app'
+# print (a,b)
+# print (a+b)
+
+# global variable 
+# a = "app"
+# def myfunc():
+#     print("hello", a)
+# myfunc()
+
+# a = "good"
+# def myfunc():
+#     a="nice"
+#     print("your website is", a)
+# myfunc()
+# print ("your website is", a)
+
+# a="good"
+# def myfunc():
+#     global a
+#     a="excellent"
+# myfunc()
+# print("this website is", a)
+
+
+# a = "app"
+# def myfun():
+#     global a
+#     a ="drew"
+# myfun()
+# print("your name is", a)
+
+# a = 13 
+# b = "app"
+# print (type(a))
+
+# a = True
+# b = False 
+# c = b"aung"
+# d = bytearray(4)
+# e = memoryview(bytes(2))
+# f = None
+
+# a = b"app"
+# b = bytearray(5)
+# c =memoryview(bytes(10))
+# d = None
+# # print (d)
+# # print (a)
+# # print (b)
+# print (c)
+
+# a = b"hello"
+# b = bytearray(10)
+# c = memoryview(bytes(10))
+# d = None
+# A = frozenset({"apple", "banana","fruit"})
+# B = {"name":"app", "age":23}
+
+# a = "app"
+# b = 10
+# c =1.1
+# d = 4l 
+# e = ["name","age","address"]
+# f = ("name","age","address")
+# g= range(10)
+# h={"name":"app","age":23}
+# i ={"name","age","address"}
+# A =frozenset({"name","age","address"})
+# B = True
+# C = b"aung"
+# D = bytearray(100)
+# E =memoryview(bytes(1000))
+# F = None
+
+
+# a = b"aung"
+# b = memoryview(bytes(120))
+# c = {"name":"app", "age": 23}
+# d = ("aung","phone","paing")
+# e = bytearray(100)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+# a = "app"
+# b = "hello" 
+# sentence = b + ' ' + a 
+# print (sentence)
+
+# a = input("your name")
+# b = int(input("your age"))
+# c = input("your current city")
+
+# print (f"Hello,{a},your name is {a},city is {c} your age is {c}")
+
+# your_age = int(input("your age!"))
+# will_be = 10 + your_age
+# print("In next 10 years, u will be", will_be)
+
+# fav_sub = input("Tell me your fav sub!")
+# print("your fav sub is", fav_sub)
